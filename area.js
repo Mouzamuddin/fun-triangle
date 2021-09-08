882bed8f7b1448c3da1ec7dcb19f85b57c8b5f00
Mouzamuddin <82389773+Mouzamuddin@users.noreply.github.com>
2021-09-08 16:13:15 +0530

    Initial commit

---
+++ area.js
@@ -0,0 +1,11 @@
+  
+var side = document.querySelectorAll('.side-length');
+var calBtn = document.querySelector("#calculate-area");
+var outputBox = document.querySelector("#output");
+
+
+function calArea() {
+    outputBox.innerText = "The Area is " + 0.5*(Number(side[0].value) * Number(side[1].value)).toFixed(2);
+}
+
+calBtn.addEventListener('click', calArea);
