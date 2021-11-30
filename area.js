6f85f5739fd4ca3419d6912f3e8a3b12261a9f35
Mouzamuddin <82389773+Mouzamuddin@users.noreply.github.com>
2021-11-30 12:39:43 +0530

    updated logic

--- area.js
+++ area.js
@@ -5,7 +5,11 @@ var outputBox = document.querySelector("#output");
 
 
 function calArea() {
+    if((Number(side[0].value) || Number(side[1].value)) < 0){
+        outputBox.innerText = "please enter positive values"
+    } else {
     outputBox.innerText = "The Area is " + 0.5*(Number(side[0].value) * Number(side[1].value)).toFixed(2);
-}
+    }
+}   
 
 calBtn.addEventListener('click', calArea);
